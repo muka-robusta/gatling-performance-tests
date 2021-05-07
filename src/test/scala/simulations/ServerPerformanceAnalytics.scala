f08167e0d9a34a749d198f2ba9bdb9016978cd9a
Debian <admin@ip-172-31-77-190.ec2.internal>
2021-05-07 19:13:58 +0000

    100 -> 1000 / sec on 1 minute

--- src/test/scala/simulations/ServerPerformanceAnalytics.scala
+++ src/test/scala/simulations/ServerPerformanceAnalytics.scala
@@ -8,7 +8,7 @@ import scala.language.postfixOps
 
 class ServerPerformanceAnalytics extends Simulation {
 
-  var httpConfig = http.baseUrl("http://3.236.113.238:8080/file-api/")
+  var httpConfig = http.baseUrl("http://18.206.194.115:8080/file-api/")
     .header("Accept", "text/plain")
 
   def getKilobyteOfData() = {
@@ -25,7 +25,7 @@ class ServerPerformanceAnalytics extends Simulation {
   setUp(
     scn.inject(
       nothingFor(1 seconds),
-      rampUsersPerSec(10) to (500) during(1 minutes)
+      rampUsersPerSec(100) to (1000) during(1 minutes)
     ).protocols(httpConfig.inferHtmlResources())
   )
 }
